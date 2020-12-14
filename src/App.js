bf9ae1ff92624ca13b5e79b0300a8c892e58e904
smishra11 <msubhasish11@gmail.com>
2020-12-14 18:53:53 +0530

    Simple CRUD operation using JSON server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  state = {
+    inputValue: '',
+    comments: [],
+  };
+  getComments = () => {
+    axios
+      .get('http://localhost:3000/comments')
+      .then((res) => this.setState({ comments: res.data }))
+      .catch((err) => console.log(err.message));
+  };
+  componentDidMount() {
+    this.getComments();
+  }
+
+  onAdd = () => {
+    if (!this.state.inputValue) {
+      return;
+    }
+    axios
+      .post('http://localhost:3000/comments', { data: this.state.inputValue })
+      .then((res) => {
+        this.getComments();
+        this.setState({ inputValue: '' });
+        console.log('GET', res.data);
+      })
+      .catch((err) => console.log(err.message));
+  };
+
+  onDelete = (id) => {
+    axios
+      .delete(`http://localhost:3000/comments/${id}`)
+      .then(() => {
+        this.getComments();
+      })
+      .catch((err) => console.log(err));
+  };
+
+  deleteAll = async () => {
+    for (let comment of this.state.comments) {
+      console.log(comment);
+      await axios
+        .delete(`http://localhost:3000/comments/${comment.id}`)
+        .then(() => {})
+        .catch((err) => console.log(err));
+    }
+    this.getComments();
+  };
+
+  render() {
+    return (
+      <div>
+        <h1
+          style={{
+            textAlign: 'center',
+            color: 'deepskyblue',
+            textDecoration: 'underline',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Comments Feed
+        </h1>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <div
+            style={{
+              width: '45%',
+            }}
+          >
+            {!this.state.comments.length ? (
+              <div style={{ color: 'red', textAlign: 'center' }}>
+                No comments Found
+              </div>
+            ) : (
+              this.state.comments.map((cmt) => {
+                return (
+                  <div
+                    key={cmt.id}
+                    style={{
+                      border: '1px solid black',
+                      marginBottom: '5px',
+                      display: 'flex',
+                      alignItems: 'center',
+                      borderRadius: '5px',
+                    }}
+                  >
+                    <button
+                      style={{ height: '25px', margin: '10px' }}
+                      onClick={() => this.onDelete(cmt.id)}
+                    >
+                      X
+                    </button>
+                    <p>{cmt.data}</p>
+                  </div>
+                );
+              })
+            )}
+          </div>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <input
+            value={this.state.inputValue}
+            placeholder="Enter comment"
+            style={{ padding: '11px' }}
+            onInput={(e) => this.setState({ inputValue: e.target.value })}
+          />
+          <button onClick={this.onAdd}>ADD</button>
+          <button style={{ marginLeft: '2rem' }} onClick={this.deleteAll}>
+            Delete All
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
